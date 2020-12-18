c33c9d054ab434519a6701ef56a1302fea298bc3
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 19:59:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     var leftNumbers = dodger.style.left.replace('px', '')
     var left = parseInt(leftNumbers, 10)
- 
+
     dodger.style.left = `${left - 1}px`
   }
 
@@ -18,4 +18,3 @@ document.addEventListener('keydown', function(e) {
     dodger.style.right = `${right - 1}px`
   }
 })
-
