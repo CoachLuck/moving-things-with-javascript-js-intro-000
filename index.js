3ac9b9ec3b0b71e11011febc5b10b479dc261801
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 19:59:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+var dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = '#FF69B4'
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+
+  if (e.which === 36) {
+    var rightNumbers = dodger.style.right.replace('px', '')
+    var right = parseInt(rightNumbers, 10)
+
+    dodger.style.right = `${right - 1}px`
+  }
+})
+
