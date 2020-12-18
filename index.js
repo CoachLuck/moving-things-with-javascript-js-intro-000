f4b594a52f28d2db98926c07a3fca1a79745c668
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 20:16:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ function moveDodgerRight() {
 }
 
 $(document).ready(function() {
-  alert('began')
-  dodger = document.getElementById('dodger');
+  dodger = $('#dodger');
   dodger.style.backgroundColor = '#FF69B4'
   dodger.style.bottom = "100px"
   $(document).on('keydown', function(e) {
